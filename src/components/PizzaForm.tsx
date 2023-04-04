802650084fb5c49081922adcc88ec33fc6dcc644
Max Gribencicov <max7gri@gmail.com>
2023-04-04 23:16:21 +0300

    Added IPizzaDetailed interface, used it in the components

--- src/components/PizzaForm.tsx
+++ src/components/PizzaForm.tsx
@@ -1,5 +1,5 @@
-import React, {FormEventHandler, useState} from 'react';
-import {IPizza} from "../data/Pizza";
+import React from 'react';
+import {IPizzaDetailed} from "../data/Pizza";
 import {Button, Form, Input, InputNumber} from 'antd';
 
 const { TextArea } = Input;
@@ -8,7 +8,7 @@ export default function PizzaForm({createPizza}: any) {
     const [form] = Form.useForm();
 
     const onSubmit = (values: any) => {
-        const newItem : IPizza={
+        const newItem : IPizzaDetailed={
             name: values.name,
             description: values.description,
             imageUrl: values.imageUrl,
@@ -28,6 +28,9 @@ export default function PizzaForm({createPizza}: any) {
             onFinish={onSubmit}
             style={{ width: "33%", margin: "1%"}}
         >
+            Add new pizza
+            <br />
+            <br />
             <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please enter name!' }]}>
                 <Input name="name" placeholder="Enter a pizza name" showCount maxLength={20}/>
             </Form.Item>
@@ -54,37 +57,5 @@ export default function PizzaForm({createPizza}: any) {
                 </Button>
             </Form.Item>
         </Form>
-
-// <form className='pizzaForm' onSubmit={handleSubmit}>
-        //     <div style={{ width: "30%", display: "inline-block", margin: "1%", padding: "1%", borderStyle: "dashed"}}>
-        //         Add new pizza
-        //         <br />
-        //         <br />
-        //         <Input value={name} showCount maxLength={20} onChange={(e)=>setName(e.target.value)} />
-        //         <br />
-        //         <br />
-        //         <TextArea value={description} showCount maxLength={300} onChange={(e)=>setDescription(e.target.value)} />
-        //         <br />
-        //         <br />
-        //         <Input value={imageUrl} placeholder="Image URL" onChange={(e)=>setImageUrl(e.target.value)} />
-        //         <br />
-        //         <br />
-        //         <InputNumber<number>
-        //             value={price}
-        //             style={{ width: 200 }}
-        //             defaultValue={1}
-        //             min={0}
-        //             max={10}
-        //             step="0.01"
-        //             precision={2}
-        //             onChange={(e)=>setPrice(e ? e : 0)}
-        //         />
-        //         <br />
-        //         <br />
-        //         <Button type="primary" size="large">
-        //             Submit
-        //         </Button>
-        //     </div>
-        // </form>
     )
 }
