8846ca416f7545e0f212a9c686a223e535a0dd9d
Max Gribencicov <max7gri@gmail.com>
2023-04-04 21:04:07 +0300

    Adjusted text on the main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is application is bootstrapped by student of group CR-203 Gribencicov Maxim
         </p>
         <a
           className="App-link"
